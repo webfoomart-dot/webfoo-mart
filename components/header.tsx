9d090a1975e2f466f36a450a3a61344e2ece6e7a
webfoomart-dot <webfoomart@gmail.com>
2026-05-04 13:11:15 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -60,7 +60,7 @@ export function Header() {
           <div className="flex items-center gap-3">
             
             {/* 🔥 YAHAN FIT KIYA HAI LIGHT/DARK MODE BUTTON 🔥 */}
-            <ThemeToggle />
+           
 
             {user && (
               <>
